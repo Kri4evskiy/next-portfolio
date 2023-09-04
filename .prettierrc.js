a8b030843ad95236481329dd49dc3bdcd7e4673c
Kri4evskiy <kri4evskiy@gmail.com>
2023-09-04 20:31:54 +0300

    configure rules

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  endOfLine: "auto",
+  bracketSpacing: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  bracketSameLine: false,
+  arrowParens: "always",
+  plugins: ["prettier-plugin-tailwindcss"]
+}
