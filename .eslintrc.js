1d531ac99f60786a943fd0eace85ab6992a1cd86
Kri4evskiy <kri4evskiy@gmail.com>
2023-09-08 15:58:03 +0300

    prettier

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+	plugins: ['sonarjs'],
+	extends: ['next/core-web-vitals', 'plugin:sonarjs/recommended', 'prettier'],
+	rules: {}
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: true,
+	compress: false,
+	productionBrowserSourceMaps: true,
+	poweredByHeader: false,
+	httpAgentOptions: {
+		keepAlive: true
+	},
+	// i18n: {
+	//   locales: ['ua', "en"],
+	//   defaultLocale: 'ua',
+	// },
+	images: {
+		formats: ['image/avif', 'image/webp']
+	},
+	experimental: {
+		typedRoutes: true
+		// urlImports: ['https://example.com/assets/', 'https://cdn.skypack.dev'],
+	}
+}
 
 module.exports = nextConfig
