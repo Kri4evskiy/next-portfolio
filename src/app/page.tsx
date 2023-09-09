d66a06c9ffbfc8df037e59a5ff4702efc9edc735
Kri4evskiy <kri4evskiy@gmail.com>
2023-09-09 11:22:01 +0300

    pre-commit configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useEffect } from 'react'
 
 export default function Home() {
+	useEffect(() => {}, [])
 	return (
 		<main className={styles.main}>
 			<div className={styles.description}>
