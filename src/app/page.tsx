b70e9a37200affabe0efc5f85e15d4961bf71a7a
Kri4evskiy <kri4evskiy@gmail.com>
2023-09-09 12:30:34 +0300

    pre-commit configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
 					<code className={styles.code}>src/app/page.tsx</code>
 				</p>
 				<div>
+					<img />
 					<a
 						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
 						target="_blank"
