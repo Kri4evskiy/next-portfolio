64e0790d36812c129d456f9b4a633b8a32622d96
Kri4evskiy <kri4evskiy@gmail.com>
2023-09-09 12:19:01 +0300

    pre-commit configurations

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -18,7 +18,7 @@ const buildEslintCommand = filenames =>
 	`next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`
 
 const buildPrettierCommand = filenames =>
-	`yarn prettier --write ${filenames.map(f => path.relative(process.cwd(), f)).join(' ')}`
+	`yarn prettier --write ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`
 
 module.exports = {
 	'*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
