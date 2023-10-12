e719f9a3e32296197a10ca8df498a2852892c673
Kri4evskiy <kri4evskiy@gmail.com>
2023-10-12 12:56:46 +0300

    investigated app structure

---
+++ src/app/(groupRoutes1)/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function About() {
+	return (
+		<>
+			<div>About</div>
+			<Link href="/">Return Home</Link>
+		</>
+	)
+}
