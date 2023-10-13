7fdbe2c326602677acaaa4c0e7940117ef4c01bc
Kri4evskiy <kri4evskiy@gmail.com>
2023-10-13 13:40:56 +0300

    app/api routes

--- src/app/(groupRoutes1)/about/page.tsx
+++ src/app/(groupRoutes1)/about/page.tsx
@@ -1,11 +1,27 @@
+'use client'
+
 import Link from 'next/link'
-import React from 'react'
+import React, { FormEvent } from 'react'
 
 export default function About() {
+	const handleSubmit = async (e: FormEvent<HTMLButtonElement>) => {
+		const res = await fetch('http://localhost:3000/api/about', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ a: 'asdasd', b: 'dsfsdfgfg' })
+		}).then(res => res.json())
+
+		console.log('res: ', res)
+	}
 	return (
 		<>
 			<div>About</div>
 			<Link href="/">Return Home</Link>
+			<button type="submit" onClick={handleSubmit}>
+				asdasdasd
+			</button>
 		</>
 	)
 }
