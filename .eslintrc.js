68bd2bcde6808864631719f3ea73ca84fc4f5d39
Kri4evskiy <kri4evskiy@gmail.com>
2023-11-23 15:37:19 +0200

    [config]: updated depth + added ts-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-	plugins: ['sonarjs', 'testing-library'],
+	plugins: ['sonarjs', 'testing-library', '@typescript-eslint'],
 	extends: [
 		'next/core-web-vitals',
 		'plugin:sonarjs/recommended',
 		'prettier',
-		'plugin:jest-dom/recommended'
+		'plugin:jest-dom/recommended',
+		'plugin:@typescript-eslint/recommended'
 	],
+	parser: '@typescript-eslint/parser',
 	rules: {
-		'jsx-a11y/alt-text': 'error'
+		'jsx-a11y/alt-text': 'error',
+		'@typescript-eslint/no-unused-vars': 'warn'
 	},
 	overrides: [
 		// Only uses Testing Library lint rules in test files
